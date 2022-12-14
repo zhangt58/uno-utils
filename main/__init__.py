92d96353f999fb0f8f049fa061015c7338c672fb
Tong Zhang <zhangt@frib.msu.edu>
2022-12-14 10:16:01 -0500

    ENH: Add a general base class for building thread workers.

--- main/__init__.py
+++ main/__init__.py
@@ -3,6 +3,7 @@ from ._base import MSG_BUTTONS, MSG_RESULTS
 from ._base import get_desktop
 from ._base import call_dispatch
 from ._range import CalcRange
+from ._thread import BaseThreadWorker
 
 
 __version__ = '0.0.1'
---
+++ main/_thread.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import threading
+
+
+class BaseThreadWorker(threading.Thread):
+    def __init__(self, name=None):
+        super().__init__()
+        self.daemon = True
+        self.paused = True
+        self.stopped = False
+        self.state = threading.Condition()
+        #
+        self.pre_run()
+        self.name = name
+
+    def pre_run(self):
+        """Initialization before run.
+        """
+        raise NotImplementedError
+
+    def is_started(self):
+        return self._started.is_set()
+
+    def is_stopped(self):
+        return self._is_stopped
+
+    def is_daemon(self):
+        return self._daemonic
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, s):
+        self.set_name(s)
+
+    def set_name(self, s):
+        if s is None:
+            self._name = threading._newname()
+        else:
+            self._name = s
+
+    def status(self):
+        if not self.is_stopped():
+            if self.paused:
+                status = "Paused"
+            else:
+                status = "Running"
+        else:
+            status = "Stopped"
+        return status
+
+    def run(self):
+        raise NotImplementedError
+
+    def stop(self):
+        with self.state:
+            self.stopped = True
+        self._wait_for_tstate_lock()
+        self._stop()
+
+    def pause(self):
+        with self.state:
+            self.paused = True
+
+    def resume(self):
+        with self.state:
+            self.paused = False
+            self.state.notify()
+
+    def __repr__(self):
+        status = self.status()
+        if self.is_daemon():
+            status += " (daemon)"
+        return f"[{self.__class__.__name__}]({self.name}@{self.ident}): {status}"
