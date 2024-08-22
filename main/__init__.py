89fe46d6e0ad3c762b5bf5ab3c266b7af4b81b73
Tong Zhang <zhangt@frib.msu.edu>
2024-08-22 16:32:49 -0400

    MNT: Bump version to 0.2.0.

--- main/__init__.py
+++ main/__init__.py
@@ -8,7 +8,7 @@ from ._thread import execute_task
 from ._utils import PVDataThread, RESULT_POOL, MDEL_DICT
 
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Tong Zhang <zhangt@frib.msu.edu>'
 __name__ = "uno_utils"
 __doc__ = """Utilities for uno development"""
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ extra_require = {
 
 setup(
     name='uno-utils',
-    version='0.1.0',
+    version='0.2.0',
     description='Utilities for uno development',
     long_description=readme(),
     long_description_content_type='text/markdown',
